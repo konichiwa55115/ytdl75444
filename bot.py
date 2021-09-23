ffea94551f28b00aeb36d47ad81cc59b7ebf73a8
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-23 11:05:31 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import logging
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+import pyrogram, os
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = pyrogram.Client(
+        "bot",
+        bot_token=os.environ.get("TOKEN"),
+        api_id=int(os.environ.get("APP_ID")),
+        api_hash=os.environ.get("API_HASH"),
+        plugins=plugins
+    )
+    app.run()
